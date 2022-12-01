ae066c6b4e3e48f78366ec96b9cd3c1ee0d2778c
ortizdiaz18 <mecatronicaeam2015@gmail.com>
2022-11-30 19:39:03 -0500

    Configuración del store en main.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import {store} from './app/store'
 import {Provider} from 'react-redux'
+import { legacy_createStore as createStore} from 'redux'
+
+const store = createStore(dogsReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
