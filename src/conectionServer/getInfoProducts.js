b0039b6b01404bbbd0a34e4be27b1ad798b71b92
HenryPB123 <mahe2di@gmail.com>
2022-12-01 18:04:05 -0500

    modificación de carpeta store

--- src/conectionServer/getInfoProducts.js
+++ src/conectionServer/getInfoProducts.js
@@ -1,3 +1,12 @@
-export default getInfoProducts = () => {
-  return fetch().then().then();
-};
+let data = [];
+
+export default function getInfoProducts() {
+  fetch(
+    `https://api.spoonacular.com/recipes/complexSearch?number=10&apiKey=8a1d871527df40a0bed857556ce13dde&addRecipeInformation=true`
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+}
+getInfoProducts();
+console.log("data", data);
