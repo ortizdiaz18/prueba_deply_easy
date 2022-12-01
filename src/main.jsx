64cbde568b51c4c72ae1ecc0070a4a195e389227
ortizdiaz18 <mecatronicaeam2015@gmail.com>
2022-11-30 19:43:53 -0500

    import de los reducers

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from './App'
 import './index.css'
 import {Provider} from 'react-redux'
 import { legacy_createStore as createStore} from 'redux'
-
-const store = createStore(dogsReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+import { reducers } from './reducers/reducers'
+const store = createStore(deucers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
