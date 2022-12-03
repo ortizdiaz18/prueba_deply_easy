e1b51a2e92d9a975d185c6ca735b2532ba8a1ce1
Yorsh355 <54050758+Yorsh355@users.noreply.github.com>
2022-12-03 17:00:15 -0500

    creación de Componente Home, carpeta images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,32 @@
 import React from "react";
-
+import s from "../styles/Home.module.css";
+import { NavBar } from ".";
 export const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <div>
+        <NavBar />
+      </div>
+      <div className={s.home__container}>
+        <div className={`${s.card} ${s.card__entradas}`}>
+          <h2>Entradas</h2>
+        </div>
+        <div className={`${s.card} ${s.card__ensaladas}`}>
+          <h2>Ensaladas</h2>
+        </div>
+        <div className={`${s.card} ${s.card__sopas}`}>
+          <h2>Sopas</h2>
+        </div>
+        <div className={`${s.card} ${s.card__principales}`}>
+          <h2>Platos principales</h2>
+        </div>
+        <div className={`${s.card} ${s.card__bebidas}`}>
+          <h2>Bebidas</h2>
+        </div>
+        <div className={`${s.card} ${s.card__postres}`}>
+          <h2>Postres</h2>
+        </div>
+      </div>
     </div>
   );
 };
