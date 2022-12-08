9b7fa00dd3b1842d37a7292a3acf3ec9c30aab78
ortizdiaz18 <96148705+ortizdiaz18@users.noreply.github.com>
2022-12-08 11:12:44 -0500

    X

--- src/redux/product/actions.js
+++ src/redux/product/actions.js
@@ -57,7 +57,7 @@ export const filterByCategory = (category) => {
   return (dispatch) =>
     axios
       .get(
-        `http://localhost:3000/api/v1//products/filterByCategory?category=${category}`
+        `http://localhost:3000/api/v1/products/filter/filterByCategory?category=${category}`
       )
       .then((response) => {
         console.log(response.data);
