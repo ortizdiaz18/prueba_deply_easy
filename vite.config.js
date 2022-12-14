d0d404300c7d9f19049699d49e34eeb9e5b0fe6e
ortizdiaz18 <96148705+ortizdiaz18@users.noreply.github.com>
2022-12-14 14:25:27 -0500

    Agrego configuracion de nuevo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig, loadEnv, preprocessCSS } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig((command, mode)=>{
+  const env = loadEnv(mode, process.cwd(),'')
+  return{
+    plugins: [react()],    
+    define:{
+      __APP_ENV__:env.APP_ENV
+    },
+    preview:{
+      port:process.env.PORT
+    } 
+   
+  }
+  
 });
