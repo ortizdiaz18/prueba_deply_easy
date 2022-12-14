8f7e4557b066579330263f5d4d25f1c8861c6114
ortizdiaz18 <96148705+ortizdiaz18@users.noreply.github.com>
2022-12-14 14:18:45 -0500

    Elimino configuracion para prueba

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,6 @@ import { defineConfig, loadEnv, preprocessCSS } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig((command, mode)=>{
-  const env = loadEnv(mode, process.cwd(),'')
-  return{
-    plugins: [react()],    
-    define:{
-      __APP_ENV__:env.APP_ENV
-    },
-    preview:{
-      port:process.env.PORT
-    } 
-   
-  }
-  
+export default defineConfig({
+  plugins: [react()],
 });
